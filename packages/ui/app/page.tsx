db578792fdbd4339c5716cefb702f94a8c49ef1b
Guilherme Luchesi <guilherme@trilon.io>
2023-05-31 23:15:31 -0300

    pipeline: final adjust

--- packages/ui/app/page.tsx
+++ packages/ui/app/page.tsx
@@ -13,7 +13,7 @@ export default async function Index() {
           <div id="welcome">
             <h1>
               <span> Hello there, </span>
-              Welcome ui 👋
+              Welcome ui! 👋
             </h1>
           </div>
 
